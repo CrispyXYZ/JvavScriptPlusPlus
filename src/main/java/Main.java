2b2db469a5b3c58d8a01085daf0eda561e64224c
CrispyXYZ <3146762331@qq.com>
2021-05-15 08:44:23 +0800

    upload files

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import  java.util.*;
+import  com.mojang.brigadier.*;
+import  com.mojang.brigadier.exceptions.*;
+import  static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
+import  static com.mojang.brigadier.builder.RequiredArgumentBuilder.argument;
+import  static com.mojang.brigadier.arguments.StringArgumentType.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Object obj = new Object();
+		Scanner scanner = new Scanner(System.in);
+		String cmd = "noEmpty";
+		CommandDispatcher<Object> dispatcher = new CommandDispatcher<>();
+		dispatcher.register(
+			literal("print")
+			.then(
+				argument("String", string())
+				.executes(c -> {
+					System.out.println(getString(c, "String"));
+					return 0;
+				})
+			)
+		);
+		dispatcher.register(
+			literal("help")
+			.then(
+				argument("command", string())
+					.executes(c -> {
+						String command = getString(c, "command");
+						for(String x: dispatcher.getSmartUsage(dispatcher.getRoot().getChild(command), obj).values())
+							System.out.printf("%s %s%n",command,x);
+						return  0;
+					})
+				)
+			.executes(c -> {
+				for(String x: dispatcher.getSmartUsage(dispatcher.getRoot(), obj).values())
+					System.out.println(x);
+				return  0;
+			})
+		);
+		System.out.print(">>>");
+		while (!(cmd = scanner.nextLine()).isEmpty()) {
+			try {
+				dispatcher.execute(cmd, obj);
+				System.out.print(">>>");
+			} catch (CommandSyntaxException e) {
+				System.out.println(e.getMessage());
+				System.out.print(">>>");
+			}
+		}
+	}
+}
