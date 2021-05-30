616ec7e462e65d6044257bacebb2b390622b3f5d
CrispyXYZ <3146762331@qq.com>
2021-05-30 16:43:57 +0800

    v0.1.1

--- src/main/java/org/jvavscript/plusplus/Main.java
+++ src/main/java/org/jvavscript/plusplus/Main.java
@@ -8,10 +8,13 @@ import  static com.mojang.brigadier.builder.RequiredArgumentBuilder.argument;
 import  static com.mojang.brigadier.arguments.StringArgumentType.*;
 
 public class Main {
+	
+	public static String VERSION="0.1.1";
+	
 	public static void main(String[] args) {
 		Object obj = new Object();
 		Scanner scanner = new Scanner(System.in);
-		String cmd = "noEmpty";
+		String cmd;
 		CommandDispatcher<Object> dispatcher = new CommandDispatcher<>();
 		dispatcher.register(
 			literal("print")
@@ -22,6 +25,20 @@ public class Main {
 					return 0;
 				})
 			)
+		);
+		dispatcher.register(
+			literal("exit")
+			.executes(c -> {
+				System.exit(0);
+				return 0;
+			})
+		);
+		dispatcher.register(
+		    literal("version")
+		    .executes( c -> {
+		    	dispatcher.execute("print "+VERSION,obj);
+		    	return 0;
+		    })
 		);
 		dispatcher.register(
 			literal("help")
@@ -40,14 +57,16 @@ public class Main {
 				return  0;
 			})
 		);
-		System.out.print(">>>");
-		while (!(cmd = scanner.nextLine()).isEmpty()) {
+		System.out.println("JvavScript++ v"+VERSION);
+		System.out.print(">>> ");
+		while (true) {
+			cmd = scanner.nextLine();
 			try {
 				dispatcher.execute(cmd, obj);
-				System.out.print(">>>");
+				System.out.print(">>> ");
 			} catch (CommandSyntaxException e) {
 				System.out.println(e.getMessage());
-				System.out.print(">>>");
+				System.out.print(">>> ");
 			}
 		}
 	}
